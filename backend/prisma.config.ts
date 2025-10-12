86ea269c4af142e9eaee003fb3188601184d8f42
GerardoFdez <101151869+GerardoFdez7@users.noreply.github.com>
2025-10-12 17:14:25 -0600

    chore: remove unused prisma config file

--- backend/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-    seed: "npx ts-node prisma/seed.ts"
-  }
-});
